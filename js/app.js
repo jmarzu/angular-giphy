45e7870f5f3bece6a723605eb06f43f244127104
Joe Marzullo <jmarzu13@gmail.com>
2016-07-24 23:36:01 -0700

    infinite search has a bug, else, the search before you finish typing works

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
-var GiphyApp = angular.module('GiphyApp', [])
-
-.run(function() {
-  console.log('App has loaded!');
-});
-
+var GiphyApp = angular.module('GiphyApp', ['infinite-scroll'])
 
 GiphyApp.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
   console.log('in the controller');
-  // $scope.search = 'seahawks';
+  // $scope.searchGifs = 'seahawks';
 
   $scope.gifSearch = function() {
     console.log($scope.searchGifs);
@@ -16,14 +11,26 @@ GiphyApp.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
         q: $scope.searchGifs,
         api_key: 'dc6zaTOxFJmzC'
       }
+    }).then(function success(res) {
+      $scope.gifs = res.data.data;
+    }, function error(res) {
+      console.log(res);
+    });
+
+    }
+
+  $scope.$watch('searchGifs', function(newVal, oldVal) {
+    $http.get('http://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: $scope.searchGifs,
+        api_key: 'dc6zaTOxFJmzC',
+        limit: 50
+      }
+    }).then(function success(res) {
+      $scope.gifs = res.data.data;
+      }, function error(res) {
+        console.log('There is an error in your search');
+      });
     })
-  .then(function success(res) {
-    $scope.gifs = res.data.data;
-  }, function error(res) {
-    console.log(res);
-  });
-
-  }
-
 
 }]);
