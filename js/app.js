77a4b250633490df10c32e83e1e10367fed8d4dd
Joe Marzullo <jmarzu13@gmail.com>
2016-07-24 22:58:40 -0700

    Working search

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-angular.module('GiphyApp', [])
+var GiphyApp = angular.module('GiphyApp', [])
 
 .run(function() {
   console.log('App has loaded!');
 });
+
+
+GiphyApp.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('in the controller');
+  // $scope.search = 'seahawks';
+
+  $scope.gifSearch = function() {
+    console.log($scope.searchGifs);
+    $http.get('http://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: $scope.searchGifs,
+        api_key: 'dc6zaTOxFJmzC'
+      }
+    })
+  .then(function success(res) {
+    $scope.gifs = res.data.data;
+  }, function error(res) {
+    console.log(res);
+  });
+
+  }
+
+
+}]);
